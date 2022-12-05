fa9f3d6a9ec6ebf16c8aa8034cf82643d706c749
kobukovu-203 <nkhauvan@gmail.com>
2022-12-05 17:28:15 +0700

    5.12.22

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 "use strict";
 
-//? SLIDE CREATE.
+//* SLIDE CREATE.
 const btnRight = document.querySelector(".slider__btn--right");
 const btnleft = document.querySelector(".slider__btn--left");
 const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dots__dot");
 let curSlide = 0;
 const maxSlide = slides.length;
 const dotContainer = document.querySelector(".dots");
+
 const creatDots = () => {
   slides.forEach((_, i) => {
     dotContainer.insertAdjacentHTML(
@@ -17,6 +17,17 @@ const creatDots = () => {
   });
 };
 creatDots();
+const dots = document.querySelectorAll(".dots__dot");
+
+//? ACTIVE DOTS
+const activeDots = (slideNumber) => {
+  dots.forEach((dot) => {
+    dot.classList.remove("dots__dot--active");
+  });
+  document
+    .querySelector(`.dots__dot[data-slide="${slideNumber}"]`)
+    .classList.add("dots__dot--active");
+};
 
 const initSlide = (slideNumbers) => {
   slides.forEach((s, i) => {
@@ -24,6 +35,7 @@ const initSlide = (slideNumbers) => {
   });
 };
 initSlide(curSlide);
+activeDots(curSlide);
 
 const nextSlide = () => {
   if (curSlide === maxSlide - 1) {
@@ -32,6 +44,8 @@ const nextSlide = () => {
     curSlide++;
   }
   initSlide(curSlide);
+  clearInterval(time);
+  activeDots(curSlide);
 };
 
 const previousSlide = () => {
@@ -41,6 +55,8 @@ const previousSlide = () => {
     curSlide--;
   }
   initSlide(curSlide);
+  activeDots(curSlide);
+  clearInterval(time);
 };
 
 btnRight.addEventListener("click", nextSlide);
@@ -52,11 +68,58 @@ document.addEventListener("keydown", (e) => {
   e.key === "ArrowLeft" && preSlide();
 });
 
-//? ACTIVE DOTS
+//? MOVE SLIDE WITH DOTS.
 dotContainer.addEventListener("click", (e) => {
-  const dotBtn = e.target.closest(".dots__dot");
-  dots.forEach((dot) => {
-    dot.classList.remove("dots__dot--active");
-  });
-  dotBtn.classList.add("dots__dot--active");
+  if (e.target.classList.contains("dots__dot")) {
+    curSlide = +e.target.dataset.slide;
+    initSlide(curSlide);
+    activeDots(curSlide);
+    clearInterval(time);
+  }
 });
+
+//? AUTOMATIC SLIDE.
+const autoSlide = () => {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  initSlide(curSlide);
+  activeDots(curSlide);
+};
+const time = setInterval(autoSlide, 3000);
+
+setTimeout(() => {
+  setInterval(autoSlide, 3000);
+}, 15000);
+
+//* STICKY HEADER.
+const header = document.querySelector(".header__container");
+const headerHeight = header.getBoundingClientRect().height;
+const slider = document.querySelector(".slider");
+const discount = document.querySelector(".discount");
+const headerSticky = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    header.classList.add("is--sticky");
+  }
+};
+const disCountRemoveSticky = (entries) => {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    header.classList.remove("is--sticky");
+  }
+};
+const headerObserver = new IntersectionObserver(headerSticky, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${-headerHeight}px`,
+});
+const discountObserver = new IntersectionObserver(disCountRemoveSticky, {
+  root: null,
+  threshold: 1,
+});
+
+headerObserver.observe(slider);
+discountObserver.observe(discount);
