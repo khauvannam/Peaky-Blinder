e95dfa4463bdab6e504494ae8f2fcf9a5ae36ca2
kobukovu-203 <nkhauvan@gmail.com>
2022-12-09 15:31:34 +0700

    9.12.22
    gulp init

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { src, dest, series, watch } = require("gulp");
+const autoPrefixer = require("gulp-autoprefixer");
+const scss = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+
+function style() {
+  return src("./sass/**/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
+    .pipe(autoPrefixer("last 2 version"))
+    .pipe(sourcemaps.write("./"))
+    .pipe(dest("./css"));
+}
+
+function watchTask() {
+  watch(["./sass/**/main.scss"], series(style));
+}
+
+exports.default = series(style, watchTask);
